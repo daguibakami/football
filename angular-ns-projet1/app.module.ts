83c89f145561f4ccc05f00d4ca78e50a2ef4b2b7
Gerard Deschamps <FR22765@fr01.awl.atosorigin.net>
2017-04-07 16:07:28 +0200

    Devenir un ninja avec angular - chap7

---
+++ angular-ns-projet1/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PonyRacerAppComponent } from './app.component';
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [PonyRacerAppComponent],
+  bootstrap: [PonyRacerAppComponent]
+})
+export class AppModule {
+}

34b87c0ef0a90384f0f49e0c490c067dfb289626
Gerard Deschamps <FR22765@fr01.awl.atosorigin.net>
2017-04-21 06:31:18 +0200

    Devenir un ninja avec angular - chap9

--- angular-ns-projet1/app.component.ts
+++ angular-ns-projet1/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RaceService } from './services/race.service';
+
 @Component({
   selector: 'ponyracer-app',
   template: `
@@ -6,10 +8,27 @@ import { Component } from '@angular/core';
   <h2>{{numberOfUsers}} users</h2>
   <h2>Welcome {{user?.name}}</h2>
   <ns-races></ns-races>
+
+  <h2>chap8</h2>
   <ns-ponies></ns-ponies>
+
+  <h2>chap9</h2>
+  <p>{{list()}}</p>
+
   `
 })
 export class PonyRacerAppComponent {
+
+  // add a constructor with RaceService
+  constructor(private raceService: RaceService) {
+  }
+  
+
   numberOfUsers = 146;
   user: any = { name: 'Cédric' };
+
+  list() {
+   return this.raceService.list();
+  }
+
 }
--- angular-ns-projet1/app.module.ts
+++ angular-ns-projet1/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PonyRacerAppComponent } from './app.component';
 import { RacesComponent } from './races.component';
 import { PoniesComponent } from './ponies.component';
+import { ApiService } from './services/api.service';
+import { RaceService } from './services/race.service';
+import { FakeRaceService } from './services/fakerace.service';
 
 @NgModule({
   imports: [BrowserModule],
   declarations: [PonyRacerAppComponent,RacesComponent,PoniesComponent],
+  providers: [
+   ApiService,
+   { provide: RaceService, useClass: FakeRaceService }
+  ],
   bootstrap: [PonyRacerAppComponent]
 })
 export class AppModule {

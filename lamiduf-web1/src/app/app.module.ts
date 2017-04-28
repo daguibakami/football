7ba66e8ace03e2d55f2354802ce17314117bf134
Dagui Bakami <dagui.bakami@gmail.com>
2017-04-28 09:51:38 +0200

    Menu Navigation (bootstrap3) + routeur angular

--- lamiduf-web1/src/app/app.module.ts
+++ lamiduf-web1/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { ActualitesComponent }  from './actualites/actualites.component';
+import { SaisonsComponent }  from './admin/saisons/saisons.component';
+import { CompetitionsComponent }  from './admin/competitions/competitions.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ActualitesComponent },
+  { path: 'actualites', component: ActualitesComponent },
+  { path: 'admin/saisons', component: SaisonsComponent },
+  { path: 'admin/competitions', component: CompetitionsComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    RouterModule.forRoot(appRoutes) 
+  ],
+  declarations: [ AppComponent,ActualitesComponent,SaisonsComponent,CompetitionsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
